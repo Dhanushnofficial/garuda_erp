85091d0ad85bf3afb1d13998608376f9885bd114
Deepakraja <dhanushnofficial@gmail.com>
2026-05-25 15:54:34 +0530

    Updated preview file name  QC

--- src/pages/QC/QCPreview.jsx
+++ src/pages/QC/QCPreview.jsx
@@ -3,6 +3,7 @@ import React, {
   useState,
   useRef,
 } from "react";
+import html2pdf from "html2pdf.js";
 
 import {
   doc,
@@ -74,10 +75,62 @@ const QCPreview = () => {
   // PRINT
   // =====================================================
 
-  const handlePrint = () => {
-    window.print();
+// =====================================================
+// DOWNLOAD PDF
+// =====================================================
+
+const handlePrint = async () => {
+
+  const element =
+    printRef.current;
+
+  if (!element) return;
+
+  const invoiceNumber =
+    data?.formData?.invoiceNo
+      ?.replace(
+        /[\/\\:*?"<>|]/g,
+        "-"
+      )
+      ?.trim();
+
+  const options = {
+
+    margin: 5,
+
+    filename:
+      `${invoiceNumber || "QC_REPORT"}.pdf`,
+
+    image: {
+      type: "jpeg",
+      quality: 1,
+    },
+
+    html2canvas: {
+      scale: 3,
+      useCORS: true,
+    },
+
+    jsPDF: {
+      unit: "mm",
+      format: "a4",
+      orientation: "portrait",
+    },
+
+    pagebreak: {
+      mode: [
+        "avoid-all",
+        "css",
+        "legacy",
+      ],
+    },
   };
 
+  await html2pdf()
+    .set(options)
+    .from(element)
+    .save();
+};
   // =====================================================
   // LOADING
   // =====================================================
